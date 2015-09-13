7d6cec108fa976b2fcf989cf01b4e61f8114aabd
Josue Rojas <josue.rojas@unitec.edu>
2015-09-13 14:01:54 -0600

    Terminando App

--- app/src/main/java/me/josuerojas/homevoicecontrol/MainActivity.java
+++ app/src/main/java/me/josuerojas/homevoicecontrol/MainActivity.java
@@ -6,12 +6,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Build;
-import android.os.ParcelUuid;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,18 +19,18 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Locale;
 import java.util.Set;
+import java.util.UUID;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private BluetoothAdapter myBluetoothAdapter;
-    private OutputStream outputStream;
-    private InputStream inStream;
+    private BluetoothDevice myDevice = null;
+    private BluetoothSocket mySocket = null;
+    private OutputStream myOutputStream = null;
     private ArrayAdapter<String> BTArrayAdapter;
     private static final int REQUEST_ENABLE_BT = 1;
     private static final int REQUEST_CODE = 1234;
@@ -54,8 +52,7 @@ public class MainActivity extends ActionBarActivity {
         });
         myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (myBluetoothAdapter == null) {
-            Toast.makeText(getApplicationContext(), "Your device does not support Bluetooth",
-                    Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Your device does not support Bluetooth", Toast.LENGTH_LONG).show();
         }
 
     }
@@ -68,7 +65,6 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-
         switch (item.getItemId()) {
             case R.id.action_btnOn:
                 onBT();
@@ -82,7 +78,6 @@ public class MainActivity extends ActionBarActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
-
     }
 
 
@@ -106,28 +101,37 @@ public class MainActivity extends ActionBarActivity {
             char firstCommand = commandFirstLevel(matches.get(0).toLowerCase());
             char secondCommand = commandSecondLevel(matches.get(0).toLowerCase());
             String text = "";
+            char sendVal = 'i';
             if (firstCommand == 'i' || secondCommand == 'i'){
                 text = "Disculpa, no te entendí.";
             } else {
                 if (firstCommand == 'a'){
                     if (secondCommand == 'a'){
                         text = "Activando Alarma";
+                        sendVal = 'e';
                     } else if (secondCommand == 'l'){
                         text = "Encendiendo Luces";
+                        sendVal = 'a';
                     } else if (secondCommand == 'p'){
                         text = "Abriendo la Puerta";
+                        sendVal = 'c';
                     } else {
                         text = "Encendiendo Ventilador";
+                        sendVal = 'g';
                     }
                 } else if(firstCommand == 'd'){
                     if (secondCommand == 'a'){
                         text = "Desactivando Alarma";
+                        sendVal = 'f';
                     } else  if (secondCommand == 'l'){
                         text = "Apagando Luces";
+                        sendVal = 'b';
                     }  else if (secondCommand == 'p'){
                         text = "Cerrando la Puerta";
+                        sendVal = 'd';
                     } else {
                         text = "Apagando Ventilador";
+                        sendVal = 'h';
                     }
                 }
             }
@@ -138,6 +142,12 @@ public class MainActivity extends ActionBarActivity {
                 tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
             }
             Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+            try {
+                myOutputStream.write(sendVal);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Error al enviar comando", Toast.LENGTH_LONG).show();
+            }
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
@@ -145,14 +155,14 @@ public class MainActivity extends ActionBarActivity {
     // retVal puede tener el valor 'a' si corresponde a un comando de a|ctivar, valor 'd' si corresponde a desactivar y valor de 'i' si esta indefinido
     protected char commandFirstLevel(String command){
         char retVal = 'i';
-        if (command.matches("^.*?(apagar|apaga|apagá|desactivar|desactiva|desactivá|cerrar|cierra|cerrá|oscurecer|oscurece|oscurecé|quitar|quita|quitá|no trabajar|no trabaje|" +
-                "no funcione|no funcionar|no función|no quiero|no querer|no quiere|silenciar|silencio|silenciá|silencia|deshace|deshacer|deshacé|apagues|apagués|desactives|" +
-                "desactivés|cierres|cerres|cerrés|oscurezcas|quites|quités|pare de trabajar|pares de trabajar|deje de funcionar|dejes de funcionar|silencies|silenciés|" +
+        if (command.matches("^.*?(apagar|apaga|apagá|apague|desactivar|desactiva|desactivá|desactive|cerrar|cierre|cierra|cerrá|oscurecer|oscurezca|oscurece|oscurecé|quitar|quite|quita|quitá|no trabajar|no trabaje|" +
+                "no funcione|no funcionar|no función|no quiero|no querer|no quiere|silenciar|silencie|silencio|silenciá|silencia|deshace|deshaga|deshacer|deshacé|apagues|apague|apagués|desactives|" +
+                "desactivés|cierres|cierra|cerres|cerrés|oscurezcas|quites|quite|quita|quitá|quités|pare de trabajar|pares de trabajar|deje de funcionar|dejes de funcionar|silencies|silenciés|" +
                 "deshagas|deshagás).*$")){
             retVal = 'd';
-        } else if (command.matches("^.*?(encender|enciende|encendé|producir|produce|produzca|haz|hace|hacé|agregar|agrega|agregá|prender|prende|prendé|activar|activa|activá|abrir|abre|abrí|" +
-                "iluminar|ilumnia|iluminá|pon|poner|poné|trabajar|trabaje|emitir|emita|emite|funcionar|funcione|función|querer|quiero|quiere|" +
-                "enciendeme|encendeme|producí|producime|haceme|hacerme|agregame|préndeme|prendeme|activame|actívame|abrime|ábreme|ilumíname|ponme|poneme|emitime).*$")){
+        } else if (command.matches("^.*?(encender|encienda|enciende|encendé|producir|produce|produzca|haz|haga|hace|hacé|agregar|agregue|agrega|agregá|prender|prenda|prende|prendé|activar|active|activa|activá|abrir|abra|abre|abrí|" +
+                "iluminar|ilumine|ilumnia|iluminá|pon|ponga|poner|poné|trabajar|trabaje|emitir|emita|emite|funcionar|funciona|funcione|función|querer|quiero|quiere|" +
+                "enciendeme|encienda|encende|encendé|encendeme|producí|produzca|producime|haceme|hagame|hacerme|agregame|agregue|préndeme|prendame|prendeme|activame|activeme|actívame|abrame|abrime|ábreme|ilumíname|ilumine|ponme|ponga|poneme|emita|emitime).*$")){
             retVal = 'a';
         }
         return retVal;
@@ -177,11 +187,9 @@ public class MainActivity extends ActionBarActivity {
         if (!myBluetoothAdapter.isEnabled()) {
             Intent turnOnIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(turnOnIntent, REQUEST_ENABLE_BT);
-            Toast.makeText(getApplicationContext(),"Bluetooth turned on" ,
-                    Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(),"Bluetooth turned on" , Toast.LENGTH_LONG).show();
         } else{
-            Toast.makeText(getApplicationContext(),"Bluetooth is already on",
-                    Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(),"Bluetooth is already on", Toast.LENGTH_LONG).show();
         }
     }
 
@@ -193,33 +201,31 @@ public class MainActivity extends ActionBarActivity {
 
     public void establishConnection(){
         if (myBluetoothAdapter.isEnabled()) {
-            Set<BluetoothDevice> bondedDevices = myBluetoothAdapter.getBondedDevices();
 
-            if(bondedDevices.size() > 0){
-                Log.e("error", String.valueOf(bondedDevices));
-                BluetoothDevice[] devices = (BluetoothDevice[]) bondedDevices.toArray();
-                BluetoothDevice device = devices[0];
-                ParcelUuid[] uuids = device.getUuids();
-                BluetoothSocket socket = null;
-                try {
-                    socket = device.createRfcommSocketToServiceRecord(uuids[0].getUuid());
-                    socket.connect();
-                    outputStream = socket.getOutputStream();
-                    inStream = socket.getInputStream();
-                } catch (IOException e) {
-                    e.printStackTrace();
+            Set <BluetoothDevice> pairedDevices = myBluetoothAdapter.getBondedDevices();
+            if(pairedDevices.size() > 0) {
+                for(BluetoothDevice device : pairedDevices) {
+                    if(device.getName().equals("HC-06")) {
+                        myDevice = device;
+                        break;
+                    }
                 }
             }
-            Toast.makeText(getApplicationContext(),"The devices has not been paired yet.",
-                    Toast.LENGTH_LONG).show();
-        }else{
-            Toast.makeText(getApplicationContext(),"Bluetooth is disabled.",
-                    Toast.LENGTH_LONG).show();
+            try {
+                UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+                mySocket = myDevice.createRfcommSocketToServiceRecord(uuid);
+                mySocket.connect();
+                myOutputStream = mySocket.getOutputStream();
+                if (mySocket.isConnected()){
+                    Toast.makeText(getApplicationContext(),"Conectado", Toast.LENGTH_SHORT).show();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(),"Error al conectar", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(),"Bluetooth is disabled.", Toast.LENGTH_LONG).show();
         }
-    }
-
-    public void write(String s) throws IOException {
-        outputStream.write(s.getBytes());
     }
 
     final BroadcastReceiver bReceiver = new BroadcastReceiver() {
@@ -236,8 +242,7 @@ public class MainActivity extends ActionBarActivity {
     public void find() {
         if (myBluetoothAdapter.isDiscovering()) {
             myBluetoothAdapter.cancelDiscovery();
-        }
-        else {
+        } else {
             BTArrayAdapter.clear();
             myBluetoothAdapter.startDiscovery();
             registerReceiver(bReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
